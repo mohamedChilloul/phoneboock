e5aea525c7b8697da3ce3c1c870bca25236607c0
mohamedChilloul <m.chilloul@esi-sba.dz>
2022-05-17 16:18:23 +0100

    phoneboock step1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App= ()=> {
+  const [persons, setPersons] = useState([{name:'chilloul mohamed'}])
+  const [name, setName] = useState('')
+  
+  const handleAdd = (event)=>{
+    event.preventDefault()
+    let newPerson = {name : name}
+    setPersons(persons.concat(newPerson))
+    setName('')
+  }
+  const nameChange = (event)=>{
+    setName(event.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phoneboock</h2>
+      <form onSubmit={handleAdd}>
+        <div>
+          <label>name : </label>
+          <input type='text' onChange={nameChange} value={name}></input>
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>persons</h2>
+      {
+        persons.map((person)=><div key={person.name}>{person.name}</div>)
+      }
     </div>
   );
 }
