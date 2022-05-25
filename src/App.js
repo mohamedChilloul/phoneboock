77ab620271fd7d3b8e5c15fd870d36af80144859
mohamedChilloul <m.chilloul@esi-sba.dz>
2022-05-25 22:20:33 +0100

    phone boock step 11 / 12

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
 import { useState, useEffect } from 'react'
 //import axios from "axios"
 import personService from './services/persons'
-const Filter = ({search, searchChange})=>{
-  return(
-    <div>
-      <label>filter Shown whith </label>
-      <input value={search} onChange={searchChange}></input>
-  </div>
-  )
-}
+import Filter from './components/filter'
+import PersonForm from './components/personForm'
+import Persons from './components/persons'
+import Notification from './components/notification'
 
-const PersonForm = (props)=>{
-  return(
-    <form onSubmit={props.handleAdd}>
-        <div>
-          <label>name : </label>
-          <input type='text' onChange={props.nameChange} value={props.name}></input>
-        </div>
-        <div>
-          <label>number : </label>
-          <input type='text' onChange={props.numberChange} value={props.number}></input>
-        </div>
-        <div>
-          <button type='submit'>add</button>
-        </div>
-      </form>
-  )
-}
-const Persons = ({persons, handleDelete})=>{
-  return(
-      persons.map((person)=>
-        <div key={person.name}>
-          {person.name} {person.number} <button onClick={()=>handleDelete(person.id)}>delete</button>
-        </div>)
-  )
-}
+
+
+
 
 const App= ()=> {
   const [persons, setPersons] = useState([])
   const [name, setName] = useState('')
   const [number, setNumber] = useState('')
   const [search, setSearch] = useState('')
+  const [message, setMessage]= useState(null)
+  const [err, setErr]= useState(false)
 
   const handleAdd = (event)=>{
     event.preventDefault()
@@ -48,21 +24,35 @@ const App= ()=> {
     let exist = names.indexOf(name)
     if (exist === -1){
       let newPerson = {name : name, number:number}
-     
       //adding the new person to the json-server
       personService.addPerson(newPerson).then(res=>{
         setPersons(persons.concat(res))
         setName('')
         setNumber('')
+        setMessage(`${newPerson.name} added successfully !`)
+        setTimeout(()=>{
+          setMessage(null)
+        },4000)
       })
     }else{
       if(window.confirm(`${name} already added to phoneBoock , do you want to replace the his number?`)){
         let exitedPers = persons.find(p =>p.name === name)
         let newPerson = {...exitedPers, number:number}
         personService.updatePerson(newPerson.id,newPerson).then(res =>{
-          setPersons(persons.map(p=>p.id!=newPerson.id ? p : res))
+          setPersons(persons.map(p=>p.id!==newPerson.id ? p : res))
           setName('')
           setNumber('')
+          setMessage(`the number of -- ${newPerson.name} -- updated successfully !`)
+          setTimeout(()=>{
+            setMessage(null)
+          },4000)
+        }).catch(err =>{
+          setErr(true)
+          setMessage(` ${newPerson.name} has been already deleted from the phoneBoock ?`)
+          setPersons(persons.filter(p=>p.id!==newPerson.id))
+          setTimeout(() => {
+            setMessage(null)
+          }, 4000)
         })
       }
     }
@@ -83,7 +73,14 @@ const App= ()=> {
     let deletedPers = persons.find(p=>p.id === id)
     if(window.confirm(`do you realy want to delete ${deletedPers.name} from the phoneBoock ?`)){
       personService.deletePerson(id).then(res=>{
-      setPersons(persons.filter(p =>p.id!=id))
+      setPersons(persons.filter(p =>p.id!==id))
+      }).catch(err =>{
+        setErr(true)
+        setMessage(` ${deletedPers.name} has been already deleted from the phoneBoock ?`)
+        setPersons(persons.filter(p=>p.id!==id))
+        setTimeout(() => {
+          setMessage(null)
+        }, 4000)
       })
     }
   }
@@ -96,6 +93,7 @@ const App= ()=> {
   return (
     <div>
       <h2>Phoneboock</h2>
+      <Notification message={message} err={err}></Notification>
       <Filter search={search} searchChange={searchChange}></Filter>
       <h2>add a new</h2>
       <PersonForm handleAdd={handleAdd}name={name}number={number}nameChange={nameChange}numberChange={numberChange}></PersonForm>
